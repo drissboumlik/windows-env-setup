514a9efae7e0f7acc5cbcba180e5c9cd244e85f9
Driss <drissboumlik.dev@gmail.com>
2025-05-24 21:45:29 +0100

    Hanlde missing folders

--- steps/install-cmder.ps1
+++ steps/install-cmder.ps1
@@ -6,27 +6,26 @@ if ($StepsQuestions["CMDER"].Answer -eq "yes") {
         $WhatToDoNext = Set-Todo-Message -message "Start cmder and Run to check for any updates : > clink update" -WhatToDoNext $WhatToDoNext
         $WhatToDoNext = Set-Todo-Message -message "Start cmder and Run 'flexprompt configure' to customize the prompt style." -WhatToDoNext $WhatToDoNext
         
-        $backupFile = "$downloadPath\Cmder\vendor\conemu-maximus5\ConEmu.xml.bak"
-        if (-not (Test-Path $backupFile)) {
-            Copy-Item -Path "$downloadPath\Cmder\vendor\conemu-maximus5\ConEmu.xml" -Destination $backupFile
+        if (Test-Path "$downloadPath\Cmder\vendor\conemu-maximus5\ConEmu.xml") {
+            Copy-Item -Path "$downloadPath\Cmder\vendor\conemu-maximus5\ConEmu.xml" -Destination "$downloadPath\Cmder\vendor\conemu-maximus5\ConEmu.xml.bak"
+        } else {
+            Write-Warning "No ConEmu.xml found to backup."
         }
         Copy-Item -Path "$PWD\config\ConEmu.xml" -Destination "$downloadPath\Cmder\vendor\conemu-maximus5\ConEmu.xml"
         
         Copy-Item -Path "$PWD\config\zoxide.lua" -Destination "$downloadPath\Cmder\config\zoxide.lua"
         
-        $backupFile = "$downloadPath\Cmder\config\user_aliases.cmd.bak"
-        if (Test-Path $backupFile) {
-            Copy-Item -Path $backupFile -Destination "$downloadPath\Cmder\config\user_aliases.cmd"
+        if (Test-Path "$downloadPath\Cmder\config\user_aliases.cmd") {
+            Copy-Item -Path "$downloadPath\Cmder\config\user_aliases.cmd" -Destination "$downloadPath\Cmder\config\user_aliases.cmd.bak"
         } else {
-            Copy-Item -Path "$downloadPath\Cmder\config\user_aliases.cmd" -Destination $backupFile
+            Write-Warning "No user_aliases.cmd found to backup."
         }
         Get-Content -Path "$PWD\config\user_aliases.cmd" | Add-Content -Path "$downloadPath\Cmder\config\user_aliases.cmd"
 
-        $backupFile = "$downloadPath\Cmder\config\user_profile.cmd.bak"
-        if (Test-Path $backupFile) {
-            Copy-Item -Path $backupFile -Destination "$downloadPath\Cmder\config\user_profile.cmd"
+        if (Test-Path "$downloadPath\Cmder\config\user_profile.cmd") {
+            Copy-Item -Path "$downloadPath\Cmder\config\user_profile.cmd" -Destination "$downloadPath\Cmder\config\user_profile.cmd.bak"
         } else {
-            Copy-Item -Path "$downloadPath\Cmder\config\user_profile.cmd" -Destination $backupFile
+            Write-Warning "No user_aliases.cmd found to backup."
         }
         Get-Content -Path "$PWD\config\user_profile.cmd" | Add-Content -Path "$downloadPath\Cmder\config\user_profile.cmd"
 

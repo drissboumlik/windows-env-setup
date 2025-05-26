a493377ea0e2e919a5f5add1ed5cc8b0bace20a3
Driss <drissboumlik.dev@gmail.com>
2025-05-26 16:54:06 +0100

    Refactor php installing with custom dir

--- steps/install-composer.ps1
+++ steps/install-composer.ps1
@@ -3,22 +3,22 @@ if ($StepsQuestions["COMPOSER"].Answer -eq "yes") {
     try {
         Write-Host "`nDownloading and installing PHP for Composer..."
         Refresh-Env
-        pvm install $USER_ENV["PHP_VERSION_TO_INSTALL"]
+        pvm install $USER_ENV["PHP_VERSION_TO_INSTALL"] "--dir=$($USER_ENV["PHP_INSTALLATION_PATH"])" > $null 2>&1
         
+        Write-Host "`nInstalling Composer..."        
+        $Global:ENV_FILE = "$PWD\.env"
         $Global:USER_ENV = Get-Env -filePath $ENV_FILE
-        $phpPath = Get-ChildItem -Path $USER_ENV['PHP_VERSIONS_PATH'] -Directory | Select-Object -First 1
-
+        $phpPath = Get-ChildItem -Path $USER_ENV["PHP_INSTALLATION_PATH"] -Directory | Select-Object -First 1
         if ($phpPath) {
-            Write-Host "`nCopying $($phpPath.FullName) to C:\php8"
-            Copy-Item -Path $phpPath.FullName -Destination "C:\php8" -Recurse
-
-            Write-Host "`nDownloading Composer..."
-            $params = '"/Php:{0}"' -f "C:\php8"
-            Write-Host "choco install composer -y --params $params"
+            Move-Item -Path "$($phpPath.FullName)\*" -Destination $USER_ENV["PHP_INSTALLATION_PATH"] -Force
+            Remove-Item -Path $phpPath.FullName -Recurse -Force
+            $params = '"/Php:{0}"' -f $phpPath
             choco install composer -y --params $params
-
-            $WhatWasDoneMessages = Set-Success-Message -message "Composer was installed successfully!" -WhatWasDoneMessages $WhatWasDoneMessages
+        } else {
+            choco install composer -y
         }
+
+        $WhatWasDoneMessages = Set-Success-Message -message "Composer was installed successfully!" -WhatWasDoneMessages $WhatWasDoneMessages
 
         $url = "https://getcomposer.org/download/1.10.27/composer.phar"
         Download-File -url $url -output "$PWD\tools\composer-v1\composer.phar"
@@ -27,7 +27,7 @@ if ($StepsQuestions["COMPOSER"].Answer -eq "yes") {
         Copy-Item -Path "$PWD\tools\composer-v1" -Destination "C:\composer\v1" -Recurse
         Update-Path-Env-Variable -variableName "C:\composer\v1" -isVarName 0
 
-        $WhatWasDoneMessages = Set-Success-Message -message "composer1 was successfully added to the PATH" -WhatWasDoneMessages $WhatWasDoneMessages
+        $WhatWasDoneMessages = Set-Success-Message -message "- composer1 was successfully added to the PATH" -WhatWasDoneMessages $WhatWasDoneMessages
     }
     catch {
         $WhatWasDoneMessages = Set-Error-Message -message "Composer failed to install, try again" -exceptionMessage $_.InvocationInfo.PositionMessage -WhatWasDoneMessages $WhatWasDoneMessages

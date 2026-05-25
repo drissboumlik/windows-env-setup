65acf90fac3b347461c66e831aa602ea8799b26a
Driss <drissboumlik.dev@gmail.com>
2026-05-25 12:18:58 +0100

    Refactor bat launchers

--- setup.bat
+++ setup.bat
@@ -3,7 +3,11 @@ setlocal
 
 rem Run PowerShell with the provided arguments
 SET FILE_TARGET=%~dp0\src\setup.ps1
-powershell -ExecutionPolicy Bypass -File "%FILE_TARGET%" %*
-@REM pwsh -ExecutionPolicy Bypass -File "%FILE_TARGET%" %*
+where pwsh >nul 2>&1
+if %ERRORLEVEL%==0 (
+    pwsh -NoProfile -ExecutionPolicy Bypass -File "%FILE_TARGET%" %ARGS%
+) else (
+    powershell -NoProfile -ExecutionPolicy Bypass -File "%FILE_TARGET%" %ARGS%
+)
 
 endlocal

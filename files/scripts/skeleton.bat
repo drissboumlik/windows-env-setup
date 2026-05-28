d0e82c565f8e5eaa659227f962ad0d0f2fd37105
Driss <drissboumlik.dev@gmail.com>
2026-05-28 20:30:10 +0100

    Fix typo

--- files/scripts/skeleton.bat
+++ files/scripts/skeleton.bat
@@ -2,7 +2,7 @@
 setlocal
 
 rem Run PowerShell with the provided arguments
-SET FILE_TARGET=%~dp0__FILE_TARGET__
+SET FILE_TARGET=__FILE_TARGET__
 powershell -ExecutionPolicy Bypass -File "%FILE_TARGET%" %*
 
 endlocal

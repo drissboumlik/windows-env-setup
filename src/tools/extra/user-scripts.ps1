fef56223c0c96768df16a7504d0d0f3280712156
Driss <drissboumlik.dev@gmail.com>
2026-06-04 00:18:15 +0100

    Fix typo

--- src/tools/extra/user-scripts.ps1
+++ src/tools/extra/user-scripts.ps1
@@ -16,7 +16,7 @@ function Install-UserScripts {
         $toolsPath = "$downloadPath\$TOOLS_INSTALLATION_DIRECTORY_PATH"
         $created = Make-Directory -path $toolsPath
         if ($created -ne 0) {
-            throw "Failed to create tools/bin directory at $toolsPath"
+            throw "Failed to create tools/bin directory at '$toolsPath'"
         }
 
         $messages = @()

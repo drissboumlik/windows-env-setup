350cf7b4a7aaf0c4618cf0ccee69d4acc73b5dc1
Driss <drissboumlik.dev@gmail.com>
2026-06-05 15:09:50 +0100

    Fix path

--- src/followup.ps1
+++ src/followup.ps1
@@ -9,7 +9,7 @@ Get-ChildItem "$PSScriptRoot\core\*.ps1" | ForEach-Object { . $_.FullName }
 Get-ChildItem "$PSScriptRoot\functions\*.ps1" | ForEach-Object { . $_.FullName }
 
 # Load Steps
-Get-ChildItem "$PSScriptRoot\steps\*.ps1" | ForEach-Object { . $_.FullName }
+Get-ChildItem "$PSScriptRoot\tools\*.ps1" -Recurse -File | ForEach-Object { . $_.FullName }
 
 
 $exitCode = Follow-Up

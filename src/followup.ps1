8ee121b055ac48d684fae8545385d913facbe948
Driss <drissboumlik.dev@gmail.com>
2026-06-14 17:22:17 +0100

    Add recurse option to load nested files

--- src/followup.ps1
+++ src/followup.ps1
@@ -2,10 +2,10 @@
 $ProgressPreference = 'SilentlyContinue'
 
 # Load functions scripts
-Get-ChildItem "$PSScriptRoot\functions\*.ps1" | ForEach-Object { . $_.FullName }
+Get-ChildItem "$PSScriptRoot\functions\*.ps1" -Recurse -File | ForEach-Object { . $_.FullName }
 
 # Load configuration
-Get-ChildItem "$PSScriptRoot\core\*.ps1" | ForEach-Object { . $_.FullName }
+Get-ChildItem "$PSScriptRoot\core\*.ps1" -Recurse -File | ForEach-Object { . $_.FullName }
 
 # Load Steps
 Get-ChildItem "$PSScriptRoot\tools\*.ps1" -Recurse -File | ForEach-Object { . $_.FullName }
